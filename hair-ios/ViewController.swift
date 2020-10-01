7858d1fcd2686e0563131bf17e68edf8870a8463
Zoli Nahoczki <nnahoczki@gmail.com>
2020-10-01 16:20:18 -0500

    Initial Commit

---
+++ hair-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hair-ios
+//
+//  Created by Zoli Nahoczki on 10/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
